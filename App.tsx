93eec853ab566fd976d2e69528f4f15a3c1e9a68
Marc Andreas Yao <marcandreas2018@gmail.com>
2025-07-26 09:24:55 +0000

    Updated provider and fixed main container size on mobile

--- App.tsx
+++ App.tsx
@@ -329,7 +329,7 @@ const Header: React.FC<{
                                     <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-5.5l4.88-4.88-1.41-1.41L10 11.68 7.71 9.39l-1.42 1.42L10 14.5z"/>
                                     </svg>
-                                    OWM
+                                    O.W.M
                                     {/* Standard (OpenWeatherMap) */}
                                 </li>
                                 <li
